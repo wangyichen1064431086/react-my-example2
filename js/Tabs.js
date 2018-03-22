60447e0aa41f69f238aaae955f08208babcb8442
wangyichen1064431086 <1064431086@qq.com>
2018-03-22 18:10:28 +0800

    Start to build and start to write CSS modules

---
+++ js/Tabs.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import CSSModules from 'react-css-modules';
+import { Seq } from 'immutable';
+import { immutableRenderDecorator } from 'react-immutable-render-mixin';
+import { Motion, spring } from 'react-motion';
+
---
+++ webpack.config.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const sassLoader = 'style-loader!css-loader?modules&importLoaders&localIdentName=[path]__[name]__[local]__[hash:base64:5]!sass-loader?sourceMap=true&sourceMapContents=true';
+
+module.exports = {
+  mode:'development',
+  devtool: 'cheap-module-eval-source-map',//This option controls if and how source maps are generated.
+  // enhance debugging by adding meta info for the browser devtools(通过为浏览器devtools添加元信息来增强调试功能)
+
+  entry: [
+    'webpack-hot-middleware/client',
+      './js/app.js'
+  ],
+  output: {
+     // options related to how webpack emits results
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+
+  module: {
+    rules: [{
+      test:/\.jsx?$/,//匹配.js和.jsx结尾的文件
+      include: [
+        path.resolve(__dirname, 'js')
+      ],
+      loaders: ['babel-loader']//Rule.loaders is an alias to Rule.use.
+    }, {
+      test: /\.scss$/,
+      include: [
+        path.resolve(__dirname, 'css')
+      ],
+      loader: sassLoader
+    }]
+  },
+
+  resolve: { 
+    //Configure how modules are resolved.
+    alias: {
+      //Create aliases to import or require certain modules more easily. Eg: in app.js, "import React from '../node_modules/react';" can now be written as "import React from 'react"
+      'react': path.join(__dirname,'node_modules','react')
+    },
+    extensions: [
+      //Enables users to leave off the extension when importing.(省略引入文件的后缀)
+      '.js', '.jsx','.scss','.css'
+    ]
+  },
+
+  plugins: [
+    //new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
